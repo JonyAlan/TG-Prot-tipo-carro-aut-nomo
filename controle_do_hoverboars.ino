c4cce8f4fe2ad1d5a96bb7ddc4f052f23fd62636
JonyAlan <34633476+JonyAlan@users.noreply.github.com>
2019-06-20 23:47:45 -0300

    sentido e velocidades

--- controle_do_hoverboars.ino
+++ controle_do_hoverboars.ino
@@ -1,10 +1,13 @@
-int motores = 6; // motor pwm
-int velocidade = 122;
-char direcao = 'F';
+int motores = 6; // motores na saida pwm
+int direcao = 5;
+int velocidade = 128;
+int lado = 116;
+char sentido = 'F';
  
 void setup() {
   Serial.begin(9600);    //configura comunicação serial com 9600 bps
   pinMode(motores,OUTPUT);   //configura como saída
+  pinMode(direcao,OUTPUT);   //configura como saída
 }
  
 void loop() {
@@ -21,37 +24,42 @@ void loop() {
         break;
         
         case 'F':            
-          velocidade = 122;
-          direcao = 'F';
+          velocidade = 126;
+          sentido = 'F';
         break;
         
-        case 'R':            
-          velocidade = 122;
-          direcao = 'R';
+        case 'T':            
+          sentido = 'T';
+        break;
+
+        case 'D':            
+          lado = 210;
+        break;
+
+        case 'E':            
+          lado = 0;
+        break;
+
+        case 'C':            
+          lado = 116;
         break;
       }
    }
-   if(strcmp(direcao,'F') == 0){
-     if(velocidade > 121 && velocidade < 141) {
+   if(strcmp(sentido,'F') == 0){
+     if(velocidade > 127 && velocidade < 148) {
        analogWrite(motores, velocidade);
      }
-     if(velocidade > 141){
-       velocidade = 140;
+     if(velocidade > 148){
+       velocidade = 147;
      }
-     if(velocidade < 121){
-       velocidade = 122;
+     if(velocidade < 127){
+       velocidade = 128;
      }
-   }else if(strcmp(direcao,'R') == 0){
-     if(velocidade > 78 && velocidade < 120){
-       analogWrite(motores, velocidade);
-     }
-     if(velocidade > 119){
-       velocidade = 119;
-     }
-     if(velocidade < 79){
-       velocidade = 79;
-     }
-     
+   }else if(strcmp(sentido,'T') == 0){
+       analogWrite(motores, 76);
+       lado = 116 
    }
+   analogWrite(direcao, lado);
+   
       
 }  
